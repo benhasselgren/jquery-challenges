338ec750558b45c5b34d0d9e6c567e789a1aef16
Ben Hasselgren <ben.nadalito@gmail.com>
2018-03-24 15:53:10 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	$("a").html(function() {
+		var id = "class='bottom_button'";
+		$("a").replaceWith("<button " + id + " >Button</button>");
+		$("button").first().attr("id", "one");
+	});
+	$("button").addClass("makeRed");
+
+	$("button").mouseover(function() {
+		$(this).removeClass("makeRed").addClass("makeBorder");
+	});
+	$(".first_button").mouseleave(function() {
+		$(this).removeClass("makeBorder").addClass("makeRed");
+	});
+	$(".first_button").click(function() {
+		$(this).siblings("p").slideToggle(500);
+	});
+	$(".second_button").click(function() {
+		$(this).siblings("p").hide(500);
+	});
+
+});
